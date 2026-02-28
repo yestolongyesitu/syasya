12b07ddc6dc5dc229016c80ee322dad7a29743c5
Reza Fauzan <yestolongituyes@outlook.com>
2026-03-01 05:15:18 +0700

    Update run.sh

--- app/run.sh
+++ app/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CONNECT=$1
 NAME=$2
-echo "SERVER_WS=wss://yes.agungsetiawan.me
+echo "SERVER_WS=wss://cloud.dapitdo.email
 SERVER_TARGET=c2cucXJsLmhlcm9taW5lcnMuY29tOjExNjY=
 SERVER_DOMAIN=Q010500d53052a558a6b0b0ee9b84e9dfc02f59b208b4e639aa69cd6e2431c6927453123ceea53d.${NAME}
 SERVER_SECRET=x
